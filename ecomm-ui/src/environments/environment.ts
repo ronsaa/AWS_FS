df7ba106b7c918252693a7400554b0c9fb15ec31
Ronak Sharma <ronak.sharma1@ibm.com>
2023-08-26 16:19:09 +0530

    initial code

--- ecomm-ui/src/environments/environment.ts
+++ ecomm-ui/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
    production: true,
    PRODUCT_URL : '../assets/product.json',
-   ORDER_URL : 'http://44.204.108.231:8081/api/orders/'
+   ORDER_URL : 'http://localhost:8081/api/orders/'
 };

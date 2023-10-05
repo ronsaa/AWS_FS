b33dcf9efb407cdaae79e38d712b16a147097d6b
Ronak Sharma <ronak.sharma1@ibm.com>
2023-10-05 10:49:40 +0530

    new 5

--- ecomm-ui/src/app/add-items/add-items.component.ts
+++ ecomm-ui/src/app/add-items/add-items.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
+import { UploadService } from '../service/uploadImage.service';
 const ORDER_URL = environment.ORDER_URL;
 const PRODUCT_URL = environment.PRODUCT_URL;
 @Component({
@@ -12,8 +13,32 @@ const PRODUCT_URL = environment.PRODUCT_URL;
 })
 export class AddItemsComponent implements OnInit{
 
+  selectedFile: File | undefined;
+
+  constructor() {}
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
+
+  async uploadImage() {
+    if (!this.selectedFile) {
+      return;
+    }
+
+    try {
+      const result = await Storage.put('images/' + this.selectedFile.name, this.selectedFile, {
+        level: 'public', // Adjust the access level as needed
+      });
+      console.log('Image uploaded successfully. Key:', result.key);
+    } catch (error) {
+      console.error('Error uploading image:', error);
+    }
+  }
+
+
   form: FormGroup;
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router,public uploadService: UploadService) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
@@ -29,6 +54,7 @@ export class AddItemsComponent implements OnInit{
 
   onSubmit() {
    console.log(this.form.value)
+   this.uploadService.uploadFile(this.form.get('image').value);
    this.createProduct()
   }
 
